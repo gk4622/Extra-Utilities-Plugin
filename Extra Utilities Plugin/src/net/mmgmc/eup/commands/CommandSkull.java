180d2c93d147f127b38845789030b3cdd7a703d3
Gleb Klepko <gleb.klepko@yahoo.com>
2021-07-20 22:36:05 -0500

    Created CommandSkull class

---
+++ Extra Utilities Plugin/src/net/mmgmc/eup/commands/CommandSkull.java
@@ -0,0 +1,16 @@
+package net.mmgmc.eup.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandSkull implements CommandExecutor
+{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
